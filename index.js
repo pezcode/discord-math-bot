ef4e52c7f363ab01d22b10a474809d5758ce39b1
pezcode <mail@rvrs.in>
2016-05-24 20:49:56 +0200

    Enforce server whitelist and blacklist

--- index.js
+++ index.js
@@ -18,10 +18,36 @@ if(config.client_id) {
     console.log('Invite URL: ' + invite_url);
 }
 
+function checkServerLists(server) {
+    // whitelist overrides blacklist
+    if(config.server_whitelist.length > 0 && config.server_whitelist.indexOf(server.id) == -1) {
+        console.log('Leaving ' + server.name + ', not on whitelist');
+        server.leave();
+    }
+    if(config.server_blacklist.indexOf(server.id) >= 0) {
+        console.log('Leaving ' + server.name + ', on blacklist');
+        server.leave();
+    }
+}
+
 client.on('ready', () => {
     me = client.user;
     console.log('Logged in as ' + me.username + '#' + me.discriminator);
     // me.id is the bot's user id, not the app's client id
+
+    client.servers.forEach( function(server) {
+        console.log('Connected to server ' + server.name + ' (' + server.id + ')');
+        checkServerLists(server);
+    });
+});
+
+client.on('serverCreated', server => {
+    console.log('Joined server ' + server.name + ' (' + server.id + ')');
+    checkServerLists(server);
+});
+
+client.on('serverDeleted', server => {
+    console.log('Left server ' + server.name + ' (' + server.id + ')');
 });
 
 client.on('disconnected', () => {
