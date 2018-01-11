fe0c7056eb3b4d9661a6b9e57e01a6fe9b06f721
pezcode <mail@rvrs.in>
2018-01-11 04:54:01 +0100

    log in and print messages

--- index.js
+++ index.js
@@ -7,30 +7,33 @@ console.log('Booting...');
 
 var closing = false;
 
-var client = new discord.Client({
-    revive : true // attempt reconnect when disconnected
-});
-
+var client = new discord.Client();
 var me = null; // the bot user
 
-if(config.client_id) {
-    //var P = discord.Constants.Permissions; // discord.Constants is null?
-    //var permissions = P.readMessages + P.sendMessages;
-    var permissions = 0x00000400 + 0x00000800; // READ_MESSAGES, SEND_MESSAGES
-    var invite_url = 'https://discordapp.com/oauth2/authorize?client_id=' + config.client_id + '&scope=bot&permissions=' + permissions;
-    console.log('Invite URL: ' + invite_url);
+client.login(config.bot_token)
+    .then(token => client.generateInvite(['VIEW_CHANNEL', 'SEND_MESSAGES']))
+    .then(link => {
+      console.log('Invite link: ' + link)
+    })
+    .catch(error => {
+        console.error('Error while logging in: ' + error.message);
+        process.exitCode = 1;
+    });
+
+function checkGuildLists(guild) {
+    // whitelist overrides blacklist
+    if(config.guild_whitelist.length > 0 && config.guild_whitelist.indexOf(guild.id) === -1) {
+        console.log('Leaving ' + guild.name + ', not on whitelist');
+        guild.leave();
+    }
+    if(config.guild_blacklist.indexOf(guild.id) !== -1) {
+        console.log('Leaving ' + guild.name + ', on blacklist');
+        guild.leave();
+    }
 }
 
-function checkServerLists(server) {
-    // whitelist overrides blacklist
-    if(config.server_whitelist.length > 0 && config.server_whitelist.indexOf(server.id) == -1) {
-        console.log('Leaving ' + server.name + ', not on whitelist');
-        server.leave();
-    }
-    if(config.server_blacklist.indexOf(server.id) >= 0) {
-        console.log('Leaving ' + server.name + ', on blacklist');
-        server.leave();
-    }
+function isOwner(user) {
+  return (config.owner_ids.indexOf(user.id) !== -1)
 }
 
 client.on('ready', () => {
@@ -38,42 +41,42 @@ client.on('ready', () => {
     console.log('Logged in as ' + me.username + '#' + me.discriminator);
     // me.id is the bot's user id, not the app's client id
 
-    client.servers.forEach( function(server) {
-        console.log('Connected to server ' + server.name + ' (' + server.id + ')');
-        checkServerLists(server);
+    client.guilds.forEach( function(guild) {
+        console.log('Connected to server ' + guild.name + ' (' + guild.id + ')');
+        checkGuildLists(guild);
     });
 });
 
-client.on('serverCreated', server => {
-    console.log('Joined server ' + server.name + ' (' + server.id + ')');
-    checkServerLists(server);
+client.on('message', message => {
+    console.log('Message from ' + message.author.tag + ': ' + message.cleanContent);
+    if (isOwner(message.author) && message.cleanContent.toLowerCase() === 'stop') {
+      client.destroy()
+    }
+});
+
+client.on('guildCreate', guild => {
+    console.log('Joined server ' + guild.name + ' (' + guild.id + ')');
+    checkServerLists(guild);
     // TODO write hello message
     // write goodbye message when leaving because banned
 });
 
-client.on('serverDeleted', server => {
-    console.log('Left server ' + server.name + ' (' + server.id + ')');
+client.on('guildDelete', guild => {
+    console.log('Left server ' + guild.name + ' (' + guild.id + ')');
 });
 
-client.on('disconnected', () => {
-    console.log('Disconnected');
-    /*
-    if (!closing) {
-        // disconnected without explicitly logging out
-        process.exitCode = 1;
-    }
-    */
+client.on('disconnect', event => {
+    console.log('Disconnected with code ' + event.code + ' (' + event.reason + ')');
+    client.destroy()
 });
 
 client.on('error', error => {
-    console.error('Fatal error: ' + error);
+    console.error('Fatal error: ' + error.message);
+});
+
+client.on('warn', info => {
+    console.log('Warning: ' + info);
 });
 
 // graceful shutdown on CTRL+C
-process.on('SIGINT', () => client.logout() );
-
-client.loginWithToken(config.bot_token)
-    .catch(error => {
-        console.error('Error while logging in: ' + error.message);
-        process.exitCode = 1;
-    });
+process.on('SIGINT', () => client.destroy() );
