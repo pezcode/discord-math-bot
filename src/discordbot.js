f19e8c535d3c10d07db6cd51c3a1e85952244cca
pezcode <mail@rvrs.in>
2018-01-14 16:51:22 +0100

    exit after failed login attempt

--- src/discordbot.js
+++ src/discordbot.js
@@ -43,7 +43,7 @@ class DiscordBot {
       })
       .catch(error => {
         console.error('Error while logging in: ' + error.message)
-        this.stop()
+        process.exit(0)
       })
   }
 
